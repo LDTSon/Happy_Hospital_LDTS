0cd8e54afaad652ed245c76aa05e0c00ed05a1fd
1LManhL1 <manh.nv204668@sis.hust.edu.vn>
2022-07-06 01:34:00 +0700

    Update Deadline cua Agv AutoAgv

--- out/production/HappyHospital-Java/src/main/ScrollBarPane.java
+++ out/production/HappyHospital-Java/src/main/ScrollBarPane.java
@@ -10,18 +10,21 @@ public class ScrollBarPane extends JFrame{
 
         public ScrollBarPane(){
 
-        JFrame frame = new JFrame("AutoAgv Deadline");
+        JFrame frame = new JFrame("AUTOAGV DEADLINE");
         frame.setLayout(new FlowLayout());
 
-        ta = new JTextArea(30, 20);
+        ta = new JTextArea(30, 36);
         ta.setLineWrap(true);
         ta.setWrapStyleWord(true);
+        ta.setFont(new Font("Monospaced", Font.ITALIC, 16));
+        ta.setForeground(Color.BLACK);
+        ta.setBackground(Color.gray);
 
         JScrollPane sp = new JScrollPane(ta, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 
         frame.add(sp);
 
-        frame.setSize(300, 600);
+        frame.setSize(400, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         }
